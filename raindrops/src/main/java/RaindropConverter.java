25120c4b51da7e45526daafda79514b2d5fc6fec
Eduardo Garcia-Castrillon <garciaed@aris-lux.lan>
2019-10-10 10:49:44 +0200

    raindrops exercise done

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,28 +1,41 @@
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 class RaindropConverter {
 
-    String convert(int number) {
+    private static final int MAX_FACTOR = 7;
 
-        String raindropSpeak = "";
-
-        List<Integer> factors = new ArrayList<>();
-
-        factors = getFactors(number, 7);
-
-        System.out.println(factors);
-
-        return "toto";
-
+    static String convert(int number) {
+        StringBuilder raindropSpeak = new StringBuilder();
+        List<Integer> factors = getFactors(number, MAX_FACTOR); // second param gives versatility
+        for (Integer factor : factors) {
+            String str = "";
+            switch (factor) {
+            case 3:
+                str = "Pling";
+                break;
+            case 5:
+                str = "Plang";
+                break;
+            case 7:
+                str = "Plong";
+                break;
+            default:
+                break;
+            }
+            raindropSpeak.append(str);
+        }
+        if (raindropSpeak.length() == 0) {
+            raindropSpeak.append(number);
+        }
+        return raindropSpeak.toString();
     }
 
-    public static void main(String[] args) {
-        RaindropConverter rc = new RaindropConverter();
-        rc.convert(34);
-    }
-
-    private List<Integer> getFactors(int number, int maxFactor) {
+    private static List<Integer> getFactors(int number, int maxFactor) {
         List<Integer> factors = new ArrayList<>();
         factors.add(1);
         for (int i = 2; i <= Math.min(number, maxFactor + 1); i++) {
@@ -31,6 +44,32 @@ class RaindropConverter {
             }
         }
         return factors;
+    }
+
+    ////////////////////////////////
+    // Most starred solution test //
+    ////////////////////////////////
+    public static void main(String[] args) {
+        int number = 345;
+        RainDrop[] testArray = RainDrop.values();
+        System.out.println(
+            Arrays.stream(testArray) // Stream<RainDrop>
+            .filter(x -> x.isFactorOf(number)) // Stream<RainDrop>
+            .map(x -> x.name()) // Stream<String>
+            .reduce(String::concat) // Optional<String>
+            .orElse(String.valueOf(number)) // String
+        );
+    }
+
+    private enum RainDrop {
+        Pling(3), Plang(5), Plong(7);        
+        private Integer idx = null;
+        RainDrop(int idx) {
+            this.idx = idx;
+        }
+        private boolean isFactorOf(int number) {
+            return number % idx == 0;
+        }
     }
 
 }
