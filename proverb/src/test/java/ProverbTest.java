8f1aab8f66756e043cf342af034488e9040f0efa
Eduardo Garcia-Castrillon <garciaed@aris-lux.lan>
2019-10-15 15:47:06 +0200

    reformatting

--- proverb/src/test/java/ProverbTest.java
+++ proverb/src/test/java/ProverbTest.java
@@ -1,8 +1,7 @@
-import org.junit.Ignore;
-import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 
 public class ProverbTest {
 
