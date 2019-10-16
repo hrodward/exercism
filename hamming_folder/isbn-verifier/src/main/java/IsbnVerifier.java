f11185fa27a1b773e83becbd48214e34bb1757ba
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-10-16 17:24:31 +0200

    isbn-verifier done with basic java

--- hamming_folder/isbn-verifier/src/main/java/IsbnVerifier.java
+++ hamming_folder/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,23 @@
 class IsbnVerifier {
 
-    boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    boolean isValid(String input) {
+        String isbn = input.replaceAll("-", "");
+        if (isbn.length() != 10) {
+            return false;
+        }
+        int sum = 0;
+        for (int idx = 0, multiplier = 10; idx < isbn.length(); idx++) {
+            String letter = String.valueOf(isbn.charAt(idx));
+            if (letter.equals("-")) {
+                continue;
+            } else if (letter.equalsIgnoreCase("X") && idx != isbn.length() - 1) {
+                return false;
+            } else if (letter.matches("[0-9Xx]")) {
+                int number = letter.equalsIgnoreCase("X") ? 10 : Integer.parseInt(letter);
+                sum += number * multiplier--;
+            }
+        }
+        return sum % 11 == 0;
     }
 
 }
