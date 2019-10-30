66ce0da59e16f76f63f195db266c6a96138cee63
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-10-30 12:12:45 +0100

    Optimized for minimal duplication

--- two_fer/beer-song/src/main/java/BeerSong.java
+++ two_fer/beer-song/src/main/java/BeerSong.java
@@ -4,15 +4,11 @@ import java.util.stream.Stream;
 
 class BeerSong {
 
-  private static final String ZERO = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n\n";
-  private static final String ONE = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\n";
-  private static final String OTHER = "%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottle%s of beer on the wall.\n\n";
+  private static final String GO = "Go to the store and buy some more";
+  private static final String TAKE = "Take one down and pass it around";
+  private static final String VERSE = "%s bottle%s of beer on the wall, %s bottle%s of beer.\n%s, %s bottle%s of beer on the wall.\n\n";
 
   private AtomicInteger counter;
-
-  public static void main(String[] args) {
-    System.out.println(new BeerSong().sing(2, 3));
-  }
 
   String singSong() {
     return sing(99, 100);
@@ -20,36 +16,17 @@ class BeerSong {
 
   String sing(int verseStart, int verses) {
     counter = new AtomicInteger(verseStart);
-    return Stream.generate(this::extracted).limit(verses).collect(Collectors.joining());
+    return Stream.generate(this::verse).limit(verses).collect(Collectors.joining());
   }
 
-  private String extracted() {
+  private String verse() {
     int index = counter.getAndDecrement();
     switch (index) {
-      case 0: return ZERO;
-      case 1: return ONE;
-      default: return String.format(OTHER, index, index, index - 1, index == 2 ? "" : "s");
+      case 0:  return String.format(VERSE, "No more", "s", "no more", "s", GO, "99", "s");
+      case 1:  return String.format(VERSE, index, "", index, "", TAKE.replace("one", "it"), "no more", "s");
+      case 2:  return String.format(VERSE, index, "s", index, "s", TAKE, index - 1, "");
+      default: return String.format(VERSE, index, "s", index, "s", TAKE, index - 1, "s");
     }
-  }
-
-  String singAlternative(int verseStart, int verses) {
-    StringBuilder sb = new StringBuilder();
-    for (int index = verseStart, counter = 0; counter < verses; index--, counter++) {
-      switch (index) {
-      case 1:
-        sb.append(ONE);
-        break;
-      case 0:
-        sb.append(ZERO);
-        break;
-      case 2:
-        sb.append(String.format(OTHER, index, index, index - 1, ""));
-        break;
-      default:
-        sb.append(String.format(OTHER, index, index, index - 1, "s"));
-      }
-    }
-    return sb.toString();
   }
 
 }
