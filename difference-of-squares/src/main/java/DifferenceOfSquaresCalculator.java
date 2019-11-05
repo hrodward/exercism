1e62a337ff69a4a1cd1b4b1bdbbffa75c02259d5
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-05 15:32:18 +0100

    difference-of-squares exercise done

---
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,23 @@
+class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Only natural integers accepted");
+        }
+        int floor = Math.floorDiv(n, 2);
+        int sumN = (n + 1) * floor + (n % 2 == 0 ? 0 : floor + 1);
+        return (int) Math.pow(sumN, 2);
+    }
+
+    int computeSumOfSquaresTo(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Only natural integers accepted");
+        }
+        return n * (n + 1) * (2 * n + 1) / 6;
+    }
+
+    int computeDifferenceOfSquares(int n) {
+        return computeSquareOfSumTo(n) - computeSumOfSquaresTo(n);
+    }
+
+}
