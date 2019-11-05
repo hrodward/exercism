f378689694aa38e392b9966c7a4cfe7342a0848b
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-05 14:10:33 +0100

    updated after feedback

--- scrabble-score/scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/scrabble-score/src/main/java/Scrabble.java
@@ -1,7 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Scrabble {
+
+    private static final Map<Character, Integer> SCORES;
+    static {
+        SCORES = new HashMap<>();
+        fill(SCORES, "AEIOULNRST", 1);
+        fill(SCORES, "DG", 2);
+        fill(SCORES, "BCMP", 3);
+        fill(SCORES, "FHVWY", 4);
+        fill(SCORES, "K", 5);
+        fill(SCORES, "JX", 8);
+        fill(SCORES, "QZ", 10);
+    }
+
+    private static void fill(Map<Character, Integer> theMap, String letters, int score) {
+        letters.chars().forEach(letter -> theMap.put((char) letter, score));
+    }
 
     private final String word;
 
@@ -10,66 +28,10 @@ class Scrabble {
     }
 
     int getScore() {
-        Stream<String> stream = Stream.of(word.toUpperCase().split(""));
-        return stream.collect(Collectors.summingInt(this::getLetterScore));
-
-        // Alternative solution:
-        // IntStream is = word.toUpperCase().codePoints().map(c -> (char) c);
-        // return is.reduce(0, (a, b) -> a + getLetterScore(String.valueOf((char) b)));
-    }
-
-    private int getLetterScore(String letter) {
-
-        int score = 0;
-
-        switch (letter) {
-        case "":
-            break;
-        case "A":
-        case "E":
-        case "I":
-        case "O":
-        case "U":
-        case "L":
-        case "N":
-        case "R":
-        case "S":
-        case "T":
-            score = 1;
-            break;
-        case "D":
-        case "G":
-            score = 2;
-            break;
-        case "B":
-        case "C":
-        case "M":
-        case "P":
-            score = 3;
-            break;
-        case "F":
-        case "H":
-        case "V":
-        case "W":
-        case "Y":
-            score = 4;
-            break;
-        case "K":
-            score = 5;
-            break;
-        case "J":
-        case "X":
-            score = 8;
-            break;
-        case "Q":
-        case "Z":
-            score = 10;
-            break;
-        default:
-            throw new IllegalArgumentException("Letter not in the abecedary");
+        if (word == null || word.length() == 0) {
+            return 0;
         }
-
-        return score;
+        Stream<String> stream = Stream.of(word.toUpperCase().split(""));
+        return stream.collect(Collectors.summingInt(s -> SCORES.get(s.charAt(0))));
     }
-
 }
