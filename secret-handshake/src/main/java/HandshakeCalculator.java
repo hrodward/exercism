d7675d85296acf74b2da4dc6463fb2a530d2eccb
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-14 11:55:03 +0100

    modified after feedback

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,39 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 class HandshakeCalculator {
 
-    private static final int REVERSE  = 0b10000;
+    private static final int REVERSE = 0b10000;
     private static final int MAX_CODE = 0b11111;
+
     private static final Signal[] SIGNALS = Signal.values();
 
     List<Signal> calculateHandshake(int number) {
-        if (number < 0 || number > MAX_CODE) {
-            throw new IllegalArgumentException("Only integers from 1 to 31 are accepted");
-        }
-        boolean isReverseOrder = number >= REVERSE;
-        if (isReverseOrder) {
-            number -= REVERSE;
-        }
-        String binaryRepresentation = Integer.toBinaryString(number);
-        AtomicInteger index = new AtomicInteger(0);
-        List<Signal> handshake = new ArrayList<>();
-        new StringBuilder(binaryRepresentation)
-            .reverse()
-            .chars()
-            .forEach(codePoint -> {
-                int currentIndex = index.getAndIncrement();
-                if((char) codePoint == '1') {
-                    Signal signal = SIGNALS[currentIndex];
-                    if (isReverseOrder) {
-                        handshake.add(0, signal);
-                    } else {
-                        handshake.add(signal);
-                    }
-                }
-            });
-        return handshake;
+	if (number < 0) {
+	    throw new IllegalArgumentException("Only positive integers are accepted");
+	}
+	if (number > MAX_CODE) {
+	    number &= MAX_CODE;
+	}
+	final boolean isReverseOrder = number >= REVERSE;
+	if (isReverseOrder) {
+	    number -= REVERSE;
+	}
+	final List<Signal> handshake = new ArrayList<>();
+	for (int idx = SIGNALS.length - 1; idx >= 0; idx--) {
+	    int code = (int) Math.pow(2, idx);
+	    if ((number & code) == code) {
+		Signal signal = SIGNALS[idx];
+		if (isReverseOrder) {
+		    handshake.add(signal);
+		} else {
+		    handshake.add(0, signal);
+		}
+	    }
+	}
+	return handshake;
     }
 
 }
