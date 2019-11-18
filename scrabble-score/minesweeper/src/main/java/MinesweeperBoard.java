548afeeab3693bddc52fc93f242c5875743d88ad
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-18 13:47:39 +0100

    minesweeper exercise done

---
+++ scrabble-score/minesweeper/src/main/java/MinesweeperBoard.java
@@ -0,0 +1,72 @@
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class MinesweeperBoard {
+	private List<String> inputBoard;
+
+	public MinesweeperBoard(List<String> inputBoard) {
+		super();
+		this.inputBoard = inputBoard;
+	}
+
+	public List<String> withNumbers() {
+
+		if (inputBoard.size() == 0) {
+			return Collections.emptyList();
+		} else if (inputBoard.size() == 1 && "".contentEquals(inputBoard.get(0))) {
+			return Collections.singletonList("");
+		}
+
+		return fillBoard(findAllMines());
+	}
+
+	private List<String> fillBoard(List<Point2D> mines) {
+		List<String> board = new ArrayList<String>();
+		for (int y = 0; y < inputBoard.size(); y++) {
+			board.add(traverseRow(y, mines));
+		}
+		return board;
+	}
+
+	private String traverseRow(int y, List<Point2D> mines) {
+		StringBuilder outputRow = new StringBuilder();
+		String[] positions = inputBoard.get(y).split("");
+
+		for (int x = 0; x < positions.length; x++) {
+			if ("*".equals(positions[x])) {
+				outputRow.append("*");
+				continue;
+			}
+			Point2D currentPosition = new Point(x, y);
+			long counter = mines.stream().filter(mine -> currentPosition.distanceSq(mine) <= 2).count();
+			outputRow.append(counter > 0 ? counter : " ");
+		}
+
+		return outputRow.toString();
+	}
+
+	private List<Point2D> findAllMines() {
+		final Point point = new Point(-1, -1);
+		return 
+				inputBoard.stream()
+					.flatMap(rowAsString -> {
+      			Stream<Point2D> rowMines = Stream.of(rowAsString.split(""))
+      					.map(position -> {
+          				point.x++;
+          				return "*".equals(position) ? new Point(point) : null;
+          			})
+      					.filter(p -> p != null)
+      					.flatMap(Stream::of);
+      			point.y++;
+      			point.x = -1;
+      			return rowMines;
+					})
+					.collect(Collectors.toList());
+	}
+
+}
