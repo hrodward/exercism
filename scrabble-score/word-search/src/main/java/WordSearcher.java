e3632748e0a14deaa22c69a1007960e808a05d2e
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-20 14:21:37 +0100

    refactor'

--- scrabble-score/word-search/src/main/java/WordSearcher.java
+++ scrabble-score/word-search/src/main/java/WordSearcher.java
@@ -74,23 +74,6 @@ class WordSearcher {
 		return new AbstractMap.SimpleEntry<>(searchWord, Optional.empty());
 	}
 
-	public static void main(final String[] args) {
-		WordSearcher wordSearcher= new WordSearcher();
-		Map<String, Optional<WordLocation>> expectedLocations = new HashMap<>();
-		expectedLocations.put("coffee", Optional.of(new WordLocation(new Pair(2, 1), new Pair(7, 1))));
-
-		Set<String> searchWords = expectedLocations.keySet();
-
-		Map<String, Optional<WordLocation>> actualLocations = wordSearcher.search(
-				searchWords,
-				new char[][]{
-					{'x', 'c', 'o', 'f', 'f', 'e', 'e', 'z', 'l', 'p'}
-				}
-				);
-
-		System.out.println(actualLocations);
-	}
-
 	private Optional<WordLocation> searchFrom(final String searchWord, final char[][] vv, final int startY, final int startX) {
 		final int limitY = vv.length;
 		final int limitX = vv[0].length;
@@ -120,36 +103,38 @@ class WordSearcher {
 	}
 
 	private Pair verifyDirection(final String searchWord, final char[][] vv, final int startY, final int startX, final Direction dir) {
-		final int limitX = vv[0].length;
 		final int limitY = vv.length;
+		final int limitX = vv[0].length;
 		AtomicInteger y = new AtomicInteger(startY);
 		AtomicInteger x = new AtomicInteger(startX);
 		AtomicInteger idx = new AtomicInteger(2);
 
-		System.out.println("Start coords: [" + startX + "," + startY + "]");
-		boolean allMatch = Stream.generate(() -> new Pair(x.addAndGet(dir.getIncrementX()), y.addAndGet(dir.getIncrementY())))
+		boolean allMatch = Stream
+			.generate(() -> new Pair(x.addAndGet(dir.getIncrementX()), y.addAndGet(dir.getIncrementY())))
 			.limit(searchWord.length() - 2)
-			.peek(pair -> System.out.println(pair))
 			.allMatch(pair -> {
-				boolean first = pair.getY() > 0 && pair.getY() <= limitY && pair.getX() > 0 && pair.getX() <= limitX;
-				if (first ) {
-					System.out.println("  first=" + first);
-					char wordChar = searchWord.charAt(idx.getAndIncrement());
-					char boardChar = vv[pair.getY() - 1][pair.getX() - 1];
-					System.out.println("    pairY=" + pair.getY() + ", pariX=" + pair.getX());
-			    boolean second = wordChar == boardChar;
-			    System.out.println("  wordChar=" + wordChar + " vs " + boardChar);
-					return first && second;
-				}
-				return false;
+		    boolean first = pair.getY() > 0 && pair.getY() <= limitY && pair.getX() > 0 && pair.getX() <= limitX;
+		    return first && searchWord.charAt(idx.getAndIncrement()) == vv[pair.getY() - 1][pair.getX() - 1];
 			});
 
-		if(allMatch) {
-			Pair pair = new Pair(x.get(), y.get());
-			System.out.println("Returning Pair" + pair);
-			return pair;
-		}
-		return null;
+		return allMatch ? new Pair(x.get(), y.get()) : null;
+	}
+
+	public static void main(final String[] args) {
+		WordSearcher wordSearcher= new WordSearcher();
+		Map<String, Optional<WordLocation>> expectedLocations = new HashMap<>();
+		expectedLocations.put("coffee", Optional.of(new WordLocation(new Pair(2, 1), new Pair(7, 1))));
+
+		Set<String> searchWords = expectedLocations.keySet();
+
+		Map<String, Optional<WordLocation>> actualLocations = wordSearcher.search(
+				searchWords,
+				new char[][]{
+					{'x', 'c', 'o', 'f', 'f', 'e', 'e', 'z', 'l', 'p'}
+				}
+				);
+
+		System.out.println(actualLocations);
 	}
 
 }
