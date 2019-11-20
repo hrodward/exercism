24a21dd75d28efd1a59a89806c1c8a6e742c0eb7
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-11-20 15:39:46 +0100

    refactor 4

--- scrabble-score/word-search/src/main/java/WordSearcher.java
+++ scrabble-score/word-search/src/main/java/WordSearcher.java
@@ -2,6 +2,7 @@ import java.util.AbstractMap;
 import java.util.EnumSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,6 +11,77 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 class WordSearcher {
+
+	private char[][] vv;
+	private String searchWord;
+
+	public Map<String, Optional<WordLocation>> search(final Set<String> searchWords, final char[][] vv) {
+		this.vv = vv;
+
+		return searchWords
+				.stream()
+				.map(this::search)
+				.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+	}
+
+	private Entry<String, Optional<WordLocation>> search(final String searchWord) {
+		this.searchWord = searchWord;
+		final char firstLetter = searchWord.charAt(0);
+
+		Optional<WordLocation> wordLocation =
+				IntStream.rangeClosed(1, vv.length)
+      	.mapToObj(x ->
+      		IntStream.rangeClosed(1, vv[x - 1].length)
+      			.filter(y -> firstLetter == vv[x - 1][y - 1])
+      			.mapToObj(y -> searchFrom(x, y))
+        		.filter(Optional::isPresent)
+      	)
+    		.flatMap(in -> in)
+    		.findFirst()
+    		.orElse(Optional.empty());
+
+		return new AbstractMap.SimpleEntry<>(searchWord, wordLocation);
+	}
+
+	private Optional<WordLocation> searchFrom(final int startX, final int startY) {
+		final int limitX = vv.length;
+		final int limitY = vv[0].length;
+		final char secondChar = searchWord.charAt(1); // first char has already been found
+
+		Optional<Pair> end =
+				IntStream.rangeClosed(startX - 1, startX + 1)
+				.mapToObj(x ->
+      		IntStream.rangeClosed(startY - 1, startY + 1)
+    		    .filter(y -> x > 0 && x <= limitX && y > 0 && y <= limitY && secondChar == vv[x - 1][y - 1])
+        		.mapToObj(y -> findEndPair(x, y, Direction.getDirectionByIncrements(x - startX, y - startY)))
+        		.filter(Objects::nonNull)
+    		)
+    		.flatMap(in -> in)
+    		.findFirst();
+
+		if (end.isPresent()) {
+			return Optional.of(new WordLocation(new Pair(startY, startX), end.get()));
+		}
+		return Optional.empty();
+	}
+
+	private Pair findEndPair(final int startY, final int startX, final Direction dir) {
+		final int limitY = vv.length;
+		final int limitX = vv[0].length;
+		final AtomicInteger x = new AtomicInteger(startX);
+		final AtomicInteger y = new AtomicInteger(startY);
+		final AtomicInteger idx = new AtomicInteger(2); // first two chars have already been checked
+
+		boolean allLettersMatch = Stream
+			.generate(() -> new Pair(x.addAndGet(dir.getIncrementY()), y.addAndGet(dir.getIncrementX())))
+			.limit(searchWord.length() - 2)
+			.allMatch(p -> {
+		    boolean isNotOutOfBoard = p.getX() > 0 && p.getX() <= limitX && p.getY() > 0 && p.getY() <= limitY;
+		    return isNotOutOfBoard && searchWord.charAt(idx.getAndIncrement()) == vv[p.getY() - 1][p.getX() - 1];
+			});
+
+		return allLettersMatch ? new Pair(x.get(), y.get()) : null;
+	}
 
 	private enum Direction {
 
@@ -47,77 +119,6 @@ class WordSearcher {
 			return null;
 		}
 
-	}
-
-	private char[][] vv;
-	private String searchWord;
-
-	public Map<String, Optional<WordLocation>> search(final Set<String> searchWords, final char[][] vv) {
-		this.vv = vv;
-
-		return searchWords
-				.stream()
-				.map(word -> search(word))
-				.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
-	}
-
-	private Entry<String, Optional<WordLocation>> search(final String searchWord) {
-		this.searchWord = searchWord;
-		char firstLetter = searchWord.charAt(0);
-
-		Optional<WordLocation> wordLocation =
-				IntStream.rangeClosed(1, vv.length)
-      	.mapToObj(x ->
-      		IntStream.rangeClosed(1, vv[x - 1].length)
-      			.filter(y -> firstLetter == vv[x - 1][y - 1])
-      			.mapToObj(y -> searchFrom(x, y))
-        		.filter(optionalWordLocation -> optionalWordLocation.isPresent())
-      	)
-    		.flatMap(in -> in)
-    		.findFirst()
-    		.orElse(Optional.empty());
-
-		return new AbstractMap.SimpleEntry<>(searchWord, wordLocation);
-	}
-
-	private Optional<WordLocation> searchFrom(final int startX, final int startY) {
-		final int limitX = vv.length;
-		final int limitY = vv[0].length;
-		final char secondChar = searchWord.charAt(1); // first char has already been found
-
-		Optional<Pair> findFirst =
-				IntStream.rangeClosed(startX - 1, startX + 1)
-				.mapToObj(x ->
-      		IntStream.rangeClosed(startY - 1, startY + 1)
-    		    .filter(y -> x > 0 && x <= limitX && y > 0 && y <= limitY && secondChar == vv[x - 1][y - 1])
-        		.mapToObj(y -> verifyDirection(x, y, Direction.getDirectionByIncrements(x - startX, y - startY)))
-        		.filter(pair -> pair != null)
-    		)
-    		.flatMap(in -> in)
-    		.findFirst();
-
-		if (findFirst.isPresent()) {
-			return Optional.of(new WordLocation(new Pair(startY, startX), findFirst.get()));
-		}
-		return Optional.empty();
-	}
-
-	private Pair verifyDirection(final int startY, final int startX, final Direction dir) {
-		final int limitY = vv.length;
-		final int limitX = vv[0].length;
-		AtomicInteger x = new AtomicInteger(startX);
-		AtomicInteger y = new AtomicInteger(startY);
-		AtomicInteger idx = new AtomicInteger(2); // first two chars have already been checked
-
-		boolean allMatch = Stream
-			.generate(() -> new Pair(x.addAndGet(dir.getIncrementY()), y.addAndGet(dir.getIncrementX())))
-			.limit(searchWord.length() - 2)
-			.allMatch(pair -> {
-		    boolean isNotOutOfBoard = pair.getX() > 0 && pair.getX() <= limitX && pair.getY() > 0 && pair.getY() <= limitY;
-		    return isNotOutOfBoard && searchWord.charAt(idx.getAndIncrement()) == vv[pair.getY() - 1][pair.getX() - 1];
-			});
-
-		return allMatch ? new Pair(x.get(), y.get()) : null;
 	}
 
 }
